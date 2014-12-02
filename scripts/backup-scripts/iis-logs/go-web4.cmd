26fd77d10010e3d16d16514671d35a67708a304f
Gerardo Santovena <gsantovena@gmail.com>
2014-12-02 11:51:55 -0600

    Cleanup!

--- scripts/backup-scripts/iis-logs/go-web4.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "c:\Sysadmin_stuff\BackupIISLogs\backup-iis-logs.ps1 -App searcher -Server web4 -DestServer zabbix.healthcare.com -Username hiflogs -Password Vantage123 -Range 2,3,4,5"
--- scripts/esb-deployment/test.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-set mydate=%date:~12,2%%date:~4,2%%date:~7,2%
-echo %mydate%
